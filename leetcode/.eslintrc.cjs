9eb0cee2fc595b20192437544bde6d112da530b5
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-06-26 22:59:11 +0530

    Configured eslint file

---
+++ leetcode/.eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', "simple-import-sort"],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'quotes': ['error', 'double'],
+    // we want to force semicolons
+    'semi': [1, 'always'],
+    "simple-import-sort/imports": "error",
+  },
+}
---
+++ leetcode/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+ <>Izhar</>
+  )
+}
+
+export default App
