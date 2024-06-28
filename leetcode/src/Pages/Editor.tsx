790d2652f6c056d0daa207397dd939b31a936ab4
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-06-29 01:31:33 +0530

    Fixed: adjusting width and height of editor

--- leetcode/src/Pages/Editor.tsx
+++ leetcode/src/Pages/Editor.tsx
@@ -31,41 +31,65 @@ Adding these numbers should give us:
 `
     const cleanMarkDown = DOMPurify.sanitize(markdown);
 
-        const[IsDragging , setIsDragging]=useState(false);
-        const[IsLeftWidth,setIsLeftWidth]=useState(80);
-        const[editorLeft,setEditorLeft] = useState(650);
+    const [IsDragging, setIsDragging] = useState(false);
+    const [DraggingVertical, setDraggingVertical] = useState(false);
+    const [IsLeftWidth, setIsLeftWidth] = useState(50);
+    const [rightWidth, setRightWidth] = useState(50);
+    const [upHeight, setUpHeight] = useState(70);
+    const [downHeight, setDownHeight] = useState(30);
+    const [editorLeft, setEditorLeft] = useState(650);
 
-            const handleMouseDown= (e : any)=>{
-                console.log(e);
-                e.preventDefault();
-                setIsDragging(true);
-            }
+    const handleMouseDown = (e: any) => {
+        console.log(e);
+        e.preventDefault();
+        setIsDragging(true);
+    }
 
-            const handleMouseUp = (e)=>{
-                console.log(e);
-                if(IsDragging){
-                    setIsDragging(false);
-                }
-            }
+    const handleMouseUp = (e: any) => {
+        console.log(e);
+        if (IsDragging) {
+            setIsDragging(false);
+        }
 
-            const dragMouseChange = (e)=>{
-                console.log('drag',IsDragging);
-                
-             if(IsDragging){
-                const newLeftWidth = (e.clientX / window.innerWidth) * 100;
-                /*  if(newLeftWidth > 10 && newLeftWidth < 90) {
-                     setIsLeftWidth(newLeftWidth);
-                 } */
-                
-                console.log('width',newLeftWidth);
-                
-                     setIsLeftWidth(newLeftWidth);  
-             }
-            }
+        if (DraggingVertical) {
+            setDraggingVertical(false);
+        }
+    }
+
+    const handleMouseUpDown = (e: any) => {
+        e.preventDefault();
+        setDraggingVertical(true);
+    }
+
+    const dragMouseChange = (e) => {
+        console.log('drag', IsDragging);
+
+        if (IsDragging) {
+            const newLeftWidth = (e.clientX / window.innerWidth) * 100;
+
+            console.log('width', newLeftWidth);
+            const newRightWidth = 100 - newLeftWidth;
+            setRightWidth(newRightWidth);
+            setIsLeftWidth(newLeftWidth);
+        }
+
+        if (DraggingVertical) {
+            const newUpHeight = (e.clientY / window.innerHeight) * 100;
+
+            console.log('width', newUpHeight);
+            const newDownHeight = 100 - newUpHeight;
+            console.log('up', newUpHeight);
+            console.log('down', newDownHeight);
+
+
+            setUpHeight(newUpHeight);
+            setDownHeight(newDownHeight);
+        }
+    }
 
     return (
         <div className='flex  h-[100vh] ' onMouseUp={handleMouseUp} onMouseMove={dragMouseChange}>
-            <div className=' p-4'  style={{ width: `${IsLeftWidth}%` }}>
+            <div className=' p-4' style={{ width: `${IsLeftWidth}%` }}>
                 <div className='flex border border-green-500'>
                     <div >Description</div>
                     <div>Solution</div>
@@ -76,37 +100,37 @@ Adding these numbers should give us:
                 </div>
             </div>
             <div className='border border-white w-2 cursor-col-resize' onMouseDown={handleMouseDown} ></div>
-            <div className='border border-violet-800 w-[90vh]' >
-                <AceEditor
-                    placeholder="Write code here"
-                    mode="javascript"
-                    theme="tomorrow_night"
-                    name="blah2"
-                    width={`${editorLeft}px`}
-                    height='725px'
-                    /*   onLoad={this.onLoad}
-                      onChange={this.onChange} */
-                    fontSize={20}
-                    lineHeight={24}
-                    showPrintMargin={true}
-                    showGutter={true}
-                    highlightActiveLine={false}
-                    focus={true}
-                    // cursorStart={1}
-                    value={`function function_name(argument) {
+            <div className=' border border-white flex flex-col' style={{ width: `${rightWidth}%` }}>
+                <div style={{ height: `${upHeight}%` }} className='border border-pink-700'>
+                    <AceEditor
+                        placeholder="Write code here"
+                        mode="javascript"
+                        theme="tomorrow_night"
+                        name="blah2"
+                        width='100%'
+                        height='100%'
+                        fontSize={20}
+                        lineHeight={24}
+                        showPrintMargin={true}
+                        showGutter={true}
+                        highlightActiveLine={false}
+                        focus={true}
+                        // cursorStart={1}
+                        value={`function function_name(argument) {
   //body...
 }`}
-                    setOptions={{
-                        enableBasicAutocompletion: true,
-                        enableLiveAutocompletion: true,
-                        enableSnippets: false,
-                        showLineNumbers: true,
-                        tabSize: 2,
-                    }} />
-            </div>
-            <div className='border border-white w-2 cursor-col-resize'   ></div>
-            <div className='border border-white w-[40vh]'>
-                submissions
+                        setOptions={{
+                            enableBasicAutocompletion: true,
+                            enableLiveAutocompletion: true,
+                            enableSnippets: false,
+                            showLineNumbers: true,
+                            tabSize: 2,
+                        }} />
+                </div>
+                <div className='border border-white w-full h-4 cursor-row-resize' onMouseDown={handleMouseUpDown} ></div>
+                <div className='border border-violet-900 w-full m-0' style={{ height: `${downHeight}%` }}>
+                    submissions
+                </div>
             </div>
         </div>
     )
